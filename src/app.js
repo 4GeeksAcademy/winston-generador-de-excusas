341a415873db271aede565d2ef4089f7ae8fe953
Wins444 <winsking04yt@gmail.com>
2024-11-29 09:13:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["El Gato", "Mi Abuelo", "EL Cartero", "Mi Guacamaya"];
+  let action = ["comió", "orinó", "aplastó", "rompió", "vomitó"];
+  let what = ["mi tarea", "mi móvil", "mi coche", "mi portátil"];
+  let when = [
+    "antes de clases",
+    "mientras dormía",
+    "cuando entrenaba",
+    "durante mi almuerzo",
+    "mientras veía netflix"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excusa").innerHTML =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
 };
